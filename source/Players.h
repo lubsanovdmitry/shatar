0f024ef7dafcf2221bf23b279da4e20da6d3add6
Dmitry Lubsanov <lubsanov11@gmail.com>
2023-03-08 20:00:17 +0800

    modified:   CMakeLists.txt 	new file:   source/Players.h

---
+++ source/Players.h
@@ -0,0 +1,20 @@
+//
+// Created by dmitry on 08.03.23.
+//
+
+#ifndef SHATAR_PLAYERS_H
+#define SHATAR_PLAYERS_H
+
+class Player {
+
+};
+
+class Human : Player {
+
+};
+
+class AI : Player {
+
+};
+
+#endif
